69ddcf84c7fd6312d9b009c0140d9ded6c3911be
Tomny <thomas.darmengeat@gmail.com>
2018-06-25 17:02:34 +0200

    Ajout du fichier d'idess :)

---
+++ MarqueurPointMolkky.mm
@@ -0,0 +1,135 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Marqueur point Molkky" FOLDED="false" ID="ID_1791343658" CREATED="1529927564747" MODIFIED="1529927580936" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties fit_to_viewport="false;"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+<edge COLOR="#ff0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+<edge COLOR="#0000ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+<edge COLOR="#00ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+<edge COLOR="#ff00ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<edge COLOR="#00ffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<edge COLOR="#7c0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<edge COLOR="#00007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<edge COLOR="#007c00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<edge COLOR="#7c007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<edge COLOR="#007c7c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<edge COLOR="#7c7c00"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Partie" POSITION="right" ID="ID_389259145" CREATED="1529927592532" MODIFIED="1529927611375">
+<edge COLOR="#ff0000"/>
+<node TEXT="Equipe" ID="ID_1997188637" CREATED="1529927642270" MODIFIED="1529927647239">
+<node TEXT="ordre joueur" ID="ID_1069462343" CREATED="1529932599901" MODIFIED="1529932606331"/>
+<node TEXT="comptabilisation global des points des joueurs de l&apos;equipe" ID="ID_402544508" CREATED="1529932621687" MODIFIED="1529932641075"/>
+</node>
+<node TEXT="Lancer" ID="ID_563897219" CREATED="1529927650515" MODIFIED="1529927661524"/>
+<node TEXT="Score" ID="ID_338215795" CREATED="1529927664823" MODIFIED="1529927667864"/>
+<node TEXT="proprietes" FOLDED="true" ID="ID_1663028762" CREATED="1529932660857" MODIFIED="1529932670207">
+<node TEXT="id" ID="ID_1925423781" CREATED="1529932671673" MODIFIED="1529934612058"/>
+<node TEXT="date" ID="ID_1924573156" CREATED="1529934613571" MODIFIED="1529934616576"/>
+<node TEXT="liste joueurs" ID="ID_735214797" CREATED="1529934617533" MODIFIED="1529934640336">
+<node TEXT="id" ID="ID_845632138" CREATED="1529934841976" MODIFIED="1529934848067"/>
+<node TEXT="lancer" ID="ID_147786142" CREATED="1529934722733" MODIFIED="1529934750587">
+<node TEXT="id" ID="ID_1405959865" CREATED="1529934855488" MODIFIED="1529934862404"/>
+<node TEXT="ordre" ID="ID_1850784392" CREATED="1529934862796" MODIFIED="1529934865667"/>
+<node TEXT="points" ID="ID_1102039361" CREATED="1529934753130" MODIFIED="1529934783305"/>
+<node TEXT="quilles tombees" ID="ID_489016044" CREATED="1529934766554" MODIFIED="1529934780074"/>
+</node>
+<node TEXT="point total" ID="ID_970614987" CREATED="1529934756857" MODIFIED="1529934764847"/>
+<node TEXT="ordre" ID="ID_1318192370" CREATED="1529934878177" MODIFIED="1529934880562"/>
+</node>
+<node TEXT="liste equipes" ID="ID_1425739700" CREATED="1529934641283" MODIFIED="1529934647215">
+<node TEXT="id" ID="ID_910073617" CREATED="1529934797429" MODIFIED="1529934814091"/>
+<node TEXT="nom" ID="ID_1745140682" CREATED="1529934814793" MODIFIED="1529934820324"/>
+<node TEXT="liste joueurs" ID="ID_1669940622" CREATED="1529934821139" MODIFIED="1529934834486"/>
+</node>
+<node TEXT="vainqueur" ID="ID_82717110" CREATED="1529934651424" MODIFIED="1529934680395"/>
+<node TEXT="duree" ID="ID_1752831296" CREATED="1529934682977" MODIFIED="1529934706384"/>
+</node>
+</node>
+<node TEXT="Joueur" POSITION="right" ID="ID_1331271930" CREATED="1529927612846" MODIFIED="1529927617148">
+<edge COLOR="#0000ff"/>
+<node TEXT="Pseudo" ID="ID_538156803" CREATED="1529927677307" MODIFIED="1529927685583"/>
+<node TEXT="stats" ID="ID_138148986" CREATED="1529927687728" MODIFIED="1529927694037"/>
+<node TEXT="groupe" ID="ID_1526018926" CREATED="1529927695524" MODIFIED="1529927705236"/>
+<node TEXT="config" ID="ID_1996257074" CREATED="1529928989716" MODIFIED="1529929004432"/>
+<node TEXT="proprietes" ID="ID_1142128939" CREATED="1529935013448" MODIFIED="1529935017505"/>
+</node>
+<node TEXT="Regle" POSITION="left" ID="ID_464811500" CREATED="1529927621337" MODIFIED="1529927625296">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="Stats" POSITION="left" ID="ID_111889892" CREATED="1529927626592" MODIFIED="1529927629407">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Nombre de parties disputees" ID="ID_148932152" CREATED="1529930380369" MODIFIED="1529930393068"/>
+<node TEXT="Quilles les plus jouees" ID="ID_444015778" CREATED="1529930398387" MODIFIED="1529930408898"/>
+<node TEXT="Meilleurs marqueurs" ID="ID_1311047000" CREATED="1529930413781" MODIFIED="1529930419650"/>
+<node TEXT="Meilleur Lanceur" ID="ID_547562708" CREATED="1529930425799" MODIFIED="1529930437272"/>
+</node>
+</node>
+</map>
